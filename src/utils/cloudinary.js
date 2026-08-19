f0d8fecd99165a45478268e15cf51767933041e7
Ridit2703 <ridit2327@gmail.com>
2026-08-19 23:31:34 +0530

    onfigure multer and cloudinary for file uploads

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from "cloudinary";
+
+import fs from "fs"
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_NAME, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+const uploadCloudinary=async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null;
+        const response=await cloudinary.uploader.upload
+        (localFilePath,{
+            resource_types:"auto"
+        })
+        console.log("file is uploaded on cloudinary",response.url)
+        return response;
+
+    }
+    catch(error){
+        fs.unlinkSync(localFilePath)//remove the locally 
+        //saved temoprary file as the upload operation got failed 
+        return null
+
+    }
+}
+    
